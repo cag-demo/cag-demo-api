c32479949bce433e3dfda45b3a0740ea38cbbb0a
Daniel Marell <daniel.marell@gmail.com>
2016-01-26 13:35:40 +0100

    Fixed autosmall deploy script.

--- api/scripts/deployandsmoke.sh
+++ api/scripts/deployandsmoke.sh
@@ -6,20 +6,20 @@ PORT=8080
 M_PORT=9200
 
 set -e
-# Newversion
+# New version
 #VERSION=${GO_PIPELINE_LABEL} (Old, may not always working futher down in the dependency chain)
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 VERSION=`grep AppVersion ${SCRIPT_DIR}/../BuildInfo.properties | awk -F'=' '{print $2}'`
 
-# Kill old instans
-echo running  docker rm -f codenight-${INSTANCE}
-docker rm -f codenight-${INSTANCE} || /bin/true
+# Kill old instances
+echo running  docker rm -f cag-demo-api-${INSTANCE}
+docker rm -f cag-demo-api-${INSTANCE} || /bin/true
 
 # Start a new one
 # Is depending on directory name running docker-compose up in!! Ugly!!
-echo running  docker run -d -e "DOODLE_ENV=${DOODLE_ENV}" --link dockerapplicationserver_elk_1:elk --name codenight-${INSTANCE} -p ${PORT_PREFIX}${PORT}:${PORT} -p ${PORT_PREFIX}${M_PORT}:${M_PORT} codenight:$VERSION
-docker run -d -e "DOODLE_ENV=${DOODLE_ENV}" --link dockerapplicationserver_elk_1:elk --name codenight-${INSTANCE} -p ${PORT_PREFIX}${PORT}:${PORT} -p ${PORT_PREFIX}${M_PORT}:${M_PORT} codenight:$VERSION
+echo running  docker run -d -e "DOODLE_ENV=${DOODLE_ENV}" --link dockerapplicationserver_elk_1:elk --name cag-demo-api-${INSTANCE} -p ${PORT_PREFIX}${PORT}:${PORT} -p ${PORT_PREFIX}${M_PORT}:${M_PORT} cag-demo-api:$VERSION
+docker run -d -e "DOODLE_ENV=${DOODLE_ENV}" --link dockerapplicationserver_elk_1:elk --name cag-demo-api-${INSTANCE} -p ${PORT_PREFIX}${PORT}:${PORT} -p ${PORT_PREFIX}${M_PORT}:${M_PORT} cag-demo-api:$VERSION
 # Sleep some time before checking that it's working
 timeout 3m bash runsmoketestuntilkilled.sh
 
--- api/scripts/smoketest.sh
+++ api/scripts/smoketest.sh
@@ -4,11 +4,11 @@ INSTANCE=$DOODLE_ENV
 PORT=8080
 
 # Get the IPAdress of the docker instans
-DOODLE_IP=`docker inspect codenight-${INSTANCE} | grep IPAddress | awk -F'\"' '{print $4}'`
+DOODLE_IP=`docker inspect cag-demo-api-${INSTANCE} | grep IPAddress | awk -F'\"' '{print $4}'`
 # Basic smoketest, TODO: should be changed to environment?
-if ! `curl -f http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
+if ! `curl -f http://${DOODLE_IP}:${PORT}/version | grep 'version:' >/dev/null`
 then
-    echo "Test failed (curl http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null)"
+    echo "Test failed (curl http://${DOODLE_IP}:${PORT}/version | grep 'version:' >/dev/null)"
     exit 1
 fi
 # More tests here!
