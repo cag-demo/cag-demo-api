10ac0d0a9d10eac5e0f1537bc22c4ba624c86ed9
Daniel Marell <daniel.marell@gmail.com>
2016-01-26 14:10:11 +0100

    Debug print.

--- api/scripts/smoketest.sh
+++ api/scripts/smoketest.sh
@@ -3,12 +3,13 @@
 INSTANCE=$PIPELINE_ENV
 PORT=8080
 
-# Get the IPAdress of the docker instans
-DOODLE_IP=`docker inspect cag-demo-api-${INSTANCE} | grep IPAddress | awk -F'\"' '{print $4}'`
+# Get the IPAdress of the docker instance
+CONTAINER_IP=`docker inspect cag-demo-api-${INSTANCE} | grep IPAddress | awk -F'\"' '{print $4}'`
+echo "CONTAINER_IP: " + $CONTAINER_IP
 # Basic smoketest, TODO: should be changed to environment?
-if ! `curl -f http://${DOODLE_IP}:${PORT}/version | grep 'version:' >/dev/null`
+if ! `curl -f http://${CONTAINER_IP}:${PORT}/version | grep 'version:' >/dev/null`
 then
-    echo "Test failed (curl http://${DOODLE_IP}:${PORT}/version | grep 'version:' >/dev/null)"
+    echo "Test failed (curl http://${CONTAINER_IP}:${PORT}/version | grep 'version:' >/dev/null)"
     exit 1
 fi
 # More tests here!
