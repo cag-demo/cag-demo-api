091784e830f6164e2afb9a5c14af4c894b1a27e4
Daniel Marell <daniel.marell@gmail.com>
2016-01-26 14:19:11 +0100

    Comment out smalltest.

--- api/scripts/runsmoketestuntilkilled.sh
+++ api/scripts/runsmoketestuntilkilled.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-while `/bin/true`
-do
-    echo try to run smoketest.sh
-    if `bash smoketest.sh >/dev/null 2>&1`; then break; fi
-    sleep 3
-done
+#while `/bin/true`
+#do
+#    echo try to run smoketest.sh
+#    if `bash smoketest.sh >/dev/null 2>&1`; then break; fi
+#    sleep 3
+#done
 
