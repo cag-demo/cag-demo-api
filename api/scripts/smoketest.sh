c6d72e870a44a2b195381c299fd53a1289031002
Daniel Marell <daniel.marell@gmail.com>
2016-02-01 17:43:26 +0100

    Debugging smoketest.sh, lagt till echo

--- api/scripts/smoketest.sh
+++ api/scripts/smoketest.sh
@@ -8,8 +8,11 @@ INSTANCE=$PIPELINE_ENV
 PORT=8080
 
 # Get ip address of the docker instance
+echo smoketest.sh debuggar #1
 CONTAINER_IP=`docker inspect cag-demo-api-${INSTANCE} | grep \"IPAddress\" | awk -F'\"' '{print $4}' | head -n 1`
+echo smoketest.sh debuggar #2
 echo smoketest.sh: CONTAINER_IP is $CONTAINER_IP
+echo smoketest.sh debuggar #3
 
 # Run basic smoke test: Call the /version endpoint and verify success.
 if ! `curl -f http://${CONTAINER_IP}:${PORT}/version | grep 'version:' >/dev/null`
