22e0b7e991b4d60d091abff06de5287662f5efb4
Daniel Marell <daniel.marell@gmail.com>
2016-02-01 18:02:19 +0100

    Debugging smoketest.sh, fixed bug in smoketest.sh

--- api/scripts/smoketest.sh
+++ api/scripts/smoketest.sh
@@ -8,16 +8,13 @@ INSTANCE=$PIPELINE_ENV
 PORT=8080
 
 # Get ip address of the docker instance
-echo smoketest.sh debuggar #1
 CONTAINER_IP=`docker inspect cag-demo-api-${INSTANCE} | grep \"IPAddress\" | awk -F'\"' '{print $4}' | head -n 1`
-echo smoketest.sh debuggar #2
-echo smoketest.sh: CONTAINER_IP is $CONTAINER_IP
-echo smoketest.sh debuggar #3
+echo smoketest.sh: CONTAINER_IP is $CONTAINER_IP, INSTANCE=$INSTANCE
 
 # Run basic smoke test: Call the /version endpoint and verify success.
-if ! `curl -f http://${CONTAINER_IP}:${PORT}/version | grep 'version:' >/dev/null`
+if ! `curl -s -f http://${CONTAINER_IP}:${PORT}/version | grep 'version' >/dev/null`
 then
-    echo "Test failed (curl http://${CONTAINER_IP}:${PORT}/version | grep 'version:' >/dev/null)"
+    echo "Test failed (curl http://${CONTAINER_IP}:${PORT}/version | grep 'version' >/dev/null)"
     exit 1
 fi
 
