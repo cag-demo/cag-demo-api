894b63ec0d9aab3f88eb50ddd489a6a3055c56b2
Daniel Marell <daniel.marell@gmail.com>
2016-02-01 17:12:44 +0100

    Debugging smoketest.sh

--- api/scripts/runsmoketestuntilkilled.sh
+++ api/scripts/runsmoketestuntilkilled.sh
@@ -9,6 +9,7 @@
 while `/bin/true`
 do
     echo try to run smoketest.sh
-    if `bash smoketest.sh >/dev/null 2>&1`; then break; fi
+    #if `bash smoketest.sh >/dev/null 2>&1`; then break; fi
+    if `bash smoketest.sh`; then break; fi
     sleep 3
 done
