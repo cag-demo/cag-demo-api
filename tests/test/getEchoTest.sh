2a24b51362f6adc97a4cd59967309aaaa989748e
Daniel Marell <daniel.marell@gmail.com>
2016-02-02 19:53:09 +0100

    Tests.

---
+++ tests/test/getEchoTest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Test REST endpoint GET /echo
+#
+API_BASE_URL=$1
+
+RESPONSE=`curl -s -f ${API_BASE_URL}/echo?s=hej`
+if [ "$RESPONSE" != "Received string: hej" ]
+then
+    echo "Test fail"
+    exit 1
+else
+    echo "Test ok"
+fi
+
+# Compact test:
+# if ! `curl -s -f ${API_BASE_URL}/echo?s=hej | grep 'hej' >/dev/null`
+#
+# Compare exact string:
+# if [ "$RESPONSE" != "Received string: hej" ]
+#
+# Compare string contains:
+# if [[ $RESPONSE != *"hej"* ]]
