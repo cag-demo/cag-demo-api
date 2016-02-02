27adf4e4b281f6e728585ec9990d00189d05a00d
KDMA02 <daniel.marell@gmail.com>
2016-02-02 16:48:00 +0100

    Support running docker both in pipeline and locally in development.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-cd target/go/docker
+APP_ENV=$1
 docker build -t cag-demo-api target/go/docker
 docker rm -f cag-demo-api &> /dev/null || true
 docker run \
-    -e SE_CAGLABS_CODENIGHT_ENVIRONMENT=prod \
+    -e SE_CAGLABS_CODENIGHT_ENVIRONMENT=APP_ENV \
     -d --name cag-demo-api \
     -p 10110:8080 \
     cag-demo-api
