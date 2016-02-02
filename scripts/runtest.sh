5121f71488427907f564a3a14aee1b41b441d264
Daniel Marell <daniel.marell@gmail.com>
2016-02-02 20:05:44 +0100

    Tests.

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -13,12 +13,11 @@ set -e
 
 # Get ip address of the docker instance
 CONTAINER_IP=`docker inspect cag-demo-api-${INSTANCE} | grep \"IPAddress\" | awk -F'\"' '{print $4}' | head -n 1`
-API_BASE_URL=http://$CONTAINER_IP:$PORT
+API_BASE_URL=http://${CONTAINER_IP}:${PORT}
 
 for f in ../tests/${INSTANCE}/*.sh
 do
-    echo Processing $f file, API_BASE_URL=API_BASE_URL ...
-    bash $f API_BASE_URL
+    echo Processing $f file using API_BASE_URL=${API_BASE_URL} ...
+    bash $f ${API_BASE_URL}
     echo Done with $f
 done
-
