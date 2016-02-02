3904f67b8a0705dd8bb9476941d43d45add20247
Daniel Marell <daniel.marell@gmail.com>
2016-02-02 19:52:57 +0100

    Modified non-pipeline deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 APP_ENV=$1
-docker build -t cag-demo-api target/go/docker
-docker rm -f cag-demo-api &> /dev/null || true
+docker build -t cag-demo-api-${APP_ENV} target/go/docker
+docker rm -f cag-demo-api-${APP_ENV} &> /dev/null || true
 docker run \
     -e SE_CAGLABS_CODENIGHT_ENVIRONMENT=APP_ENV \
-    -d --name cag-demo-api \
+    -d --name cag-demo-api-${APP_ENV} \
     -p 10110:8080 \
-    cag-demo-api
+    cag-demo-api-${APP_ENV}
