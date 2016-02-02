5199c54a1a50ae0c131daefc06d60d3fcda12020
Daniel Marell <daniel.marell@gmail.com>
2016-02-02 13:20:33 +0100

    Jenkins deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd target/go/docker
-docker build -t cag-demo-api .
+docker build -t cag-demo-api target/go/docker
 docker rm -f cag-demo-api &> /dev/null || true
 docker run \
     -e SE_CAGLABS_CODENIGHT_ENVIRONMENT=prod \
